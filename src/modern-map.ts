710a205fb301a6a8f7b00bd0c61f3bf49dedcf2b
Jungwoo An <o1o9814@naver.com>
2018-11-15 19:20:19 +0900

    pure object 와 같은 문법 지원

--- src/modern-map.ts
+++ src/modern-map.ts
@@ -1,10 +1,33 @@
 class ModernMap<K, V> {
-  private _map: Map<K, V>
+  private _map: Map<string, V>
   private _limit: number
 
   constructor(limit: number) {
-    this._map = new Map<K, V>()
+    this._map = new Map<string, V>()
     this._limit = limit
+
+    return new Proxy(this, {
+      has(target, prop: any) {
+        return target.has(prop)
+      },
+
+      set(target, prop: any, value) {
+        target.set(prop, value)
+        return true
+      },
+
+      get(target, prop: any) {
+        if (prop === 'length') {
+          return target.length
+        }
+
+        return target.get(prop)
+      }
+    })
+  }
+
+  public has(key: K) {
+    return this._map.has(key.toString())
   }
 
   public set(key: K, value: V) {
@@ -12,14 +35,16 @@ class ModernMap<K, V> {
       this._map.delete(this._map.keys().next().value)
     }
 
-    this._map.delete(key)
-    this._map.set(key, value)
+    const keyName = key.toString()
+
+    this._map.delete(keyName)
+    this._map.set(keyName, value)
 
     return this
   }
 
   public get(key: K) {
-    return this._map.get(key)
+    return this._map.get(key.toString())
   }
 
   public get length() {
--- test/modern-map.spec.ts
+++ test/modern-map.spec.ts
@@ -1,17 +1,31 @@
 import Map from '../src/modern-map'
 
+test('should usable of syntax like pure object', () => {
+  const map: any = new Map(1)
+  map[1] = 1
+
+  expect(map[1]).toEqual(1)
+})
+
+test('should usable of in keyword', () => {
+  const map: any = new Map(1)
+  map[1] = 1
+
+  expect(1 in map).toBeTruthy()
+})
+
 test('should delete by old key', () => {
-  const map = new Map(2)
+  const map: any = new Map(2)
 
-  map.set(1, 1)
-  map.set(2, 2)
+  map[1] = 1
+  map[2] = 2
 
   expect(map.length).toEqual(2)
-  expect(map.get(1)).toEqual(1)
+  expect(map[1]).toEqual(1)
 
-  map.set(3, 3)
+  map[3] = 3
   expect(map.length).toEqual(2)
-  expect(map.get(1)).toBeFalsy()
-  expect(map.get(2)).toEqual(2)
-  expect(map.get(3)).toEqual(3)
+  expect(map[1]).toBeFalsy()
+  expect(map[2]).toEqual(2)
+  expect(map[3]).toEqual(3)
 })
